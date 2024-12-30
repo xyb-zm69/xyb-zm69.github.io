93749643343507bbbd2c69d6fefa4f6586b30eb7
xyb-zm69 <xybzm69@gmail.com>
2024-12-31 00:23:57 +0800

    Site updated: 2024-12-31 00:23:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/30/My-New-Post/","2024/12/29/hello-world/"];function toRandomPost(){
+var posts=["2024/12/29/hello-world/","2024/12/30/My-New-Post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
