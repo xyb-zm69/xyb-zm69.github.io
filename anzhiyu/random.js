31c58cf39ff6d21a526538a203e8857cb36606b7
xyb-zm69 <xybzm69@gmail.com>
2024-12-31 02:05:47 +0800

    Site updated: 2024-12-31 02:05:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/29/hello-world/","2024/12/30/My-New-Post/"];function toRandomPost(){
+var posts=["2024/12/31/这是一篇新文章/","2024/12/30/My-New-Post/","2024/12/29/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
