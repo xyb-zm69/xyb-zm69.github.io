15c661005d0dfa1486edbedf642d008d3d554f5f
xyb-zm69 <xybzm69@gmail.com>
2025-01-01 02:54:09 +0800

    Site updated: 2025-01-01 02:54:09

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/这是一篇新文章/","wenzhang/My-New-Post/","wenzhang/hello-world/"];function toRandomPost(){
+var posts=["wenzhang/这是一篇新文章/","wenzhang/kkkkk/","wenzhang/前面/","wenzhang/My-New-Post/","wenzhang/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
