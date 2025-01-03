906f1b45fe20e3b01be2c5019a4b532e9e69db3c
xyb-zm69 <xybzm69@gmail.com>
2025-01-04 01:38:40 +0800

    Site updated: 2025-01-04 01:38:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["wenzhang/这是一篇新文章/","wenzhang/kkkkk/","wenzhang/前面/","wenzhang/My-New-Post/","wenzhang/hello-world/"];function toRandomPost(){
+var posts=["wenzhang/这是一篇新文章/","wenzhang/五爱市场一游/","wenzhang/测试/","wenzhang/电脑CMD下查找文件命令又快又好用/","wenzhang/kkkkk/","wenzhang/前面/","wenzhang/My-New-Post/","wenzhang/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
